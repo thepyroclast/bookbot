6749cec44980c1e80daaf4ee10a90105cf39108a
thepyroclast <colin555huff@gmail.com>
2025-11-10 16:24:08 -0700

    modified main.py to do print book

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("greetings boots") 
+def get_book_text(file_path):
+    with open(file_path) as f:
+        book_text = f.read()
+    return book_text
+
+def main():
+    print(get_book_text("./books/frankenstein.txt"))
+    return
+
+main()    
